959030edf215e040b4b37c2d701445fbb67c726d
Charlie Wilson <charlie.wilson4@gmail.com>
2023-12-11 21:11:28 +1100

    feat(11): complete day 11 part 2

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,51 +1,53 @@
 from itertools import combinations
 
-import numpy as np
-
 from solutions.base import BaseSolution
 
-Coord = tuple[int, int]
 
-
-def manhattan_distance(a: Coord, b: Coord) -> int:
-    return sum(abs(a[i] - b[i]) for i in range(len(a)))
-
-
-def expand_empty_space(raw_map: list[list[str]]) -> list[list[str]]:
-    np_map = np.array(raw_map)
-
-    # expand rows
-    rows_to_insert = [i for i, row in enumerate(np_map) if "#" not in row]
-    inserts = 0
-    for i in rows_to_insert:
-        np_map = np.insert(np_map, i + inserts + 1, np_map[i + inserts], axis=0)
-        inserts += 1
-
-    # expand columns
-    cols_to_insert = [i for i, col in enumerate(np_map.T) if "#" not in col]
-    inserts = 0
-    for i in cols_to_insert:
-        np_map = np.insert(np_map, i + inserts + 1, np_map[:, i + inserts], axis=1)
-        inserts += 1
-
-    # convert back to list of lists
-    return np_map.tolist()  # type: ignore[no-any-return]
+def get_empty_rows_and_columns(map_: list[list[str]]) -> tuple[set[int], set[int]]:
+    empty_rows = {i for i, row in enumerate(map_) if all(value == "." for value in row)}
+    empty_columns = {
+        j for j in range(len(map_[0])) if all(row[j] == "." for row in map_)
+    }
+    return empty_rows, empty_columns
 
 
 class Solution(BaseSolution):
+    def count_steps(
+        self, start: tuple[int, int], end: tuple[int, int]
+    ) -> tuple[int, int]:
+        steps = 0
+        empty_steps = 0
+        for y in range(min(start[0], end[0]), max(start[0], end[0])):
+            if y in self.empty_rows:
+                empty_steps += 1
+            else:
+                steps += 1
+        for x in range(min(start[1], end[1]), max(start[1], end[1])):
+            if x in self.empty_columns:
+                empty_steps += 1
+            else:
+                steps += 1
+        return steps, empty_steps
+
     def setup(self) -> None:
         base_map = [list(row) for row in self.raw_input.splitlines()]
-        self.map = expand_empty_space(base_map)
+        self.empty_rows, self.empty_columns = get_empty_rows_and_columns(base_map)
 
-    def part_1(self) -> int:
-        galaxies = []
-        for i, row in enumerate(self.map):
+        self.galaxies = []
+        for i, row in enumerate(base_map):
             for j, value in enumerate(row):
                 if value == "#":
-                    galaxies.append((i, j))
+                    self.galaxies.append((i, j))
 
-        pairs = combinations(galaxies, 2)
-        return sum(manhattan_distance(*pair) for pair in pairs)
+        self.num_steps = 0
+        self.num_empty_steps = 0
+        for start, end in combinations(self.galaxies, 2):
+            steps, empty_steps = self.count_steps(start, end)
+            self.num_steps += steps
+            self.num_empty_steps += empty_steps
+
+    def part_1(self) -> int:
+        return self.num_steps + (self.num_empty_steps * 2)
 
     def part_2(self) -> int:
-        return -1
+        return self.num_steps + (self.num_empty_steps * 1_000_000)
